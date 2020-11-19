c10f419e703999aeb7bbdc3eede5eee204122da8
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-11-18 22:35:25 -0300

    Messbare Implementierung für einfache for-Schleife

---
+++ LoopsPerformanceComparison/SimpleForLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Benchmarking;
+
+namespace LoopsPerformanceComparison
+{
+    /// <summary>
+    /// Ermöglicht die Messung der Leistung einer einfachen for-Schleife.
+    /// </summary>
+    class SimpleForLoop : IBenchmarkable
+    {
+        public string Name { get { return "Einfach for-Schleife"; } }
+
+        public void Run(uint levelOfLoad)
+        {
+            var data = new int[levelOfLoad];
+            int count = 0;
+            for (int idx = 0; idx < data.Length; ++idx)
+            {
+                if (data[idx] == 0)
+                {
+                    ++count;
+                }
+            }
+        }
+    }
+}
