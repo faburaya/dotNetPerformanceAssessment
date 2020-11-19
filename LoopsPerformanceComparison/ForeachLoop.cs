e237744fae0d7cb74bb65f74270a195361be04fa
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-11-18 22:34:32 -0300

    Messbare Implementierung für foreach-Schleife

---
+++ LoopsPerformanceComparison/ForeachLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Benchmarking;
+
+namespace LoopsPerformanceComparison
+{
+    /// <summary>
+    /// Ermöglicht die Messung der Leistung einer foreach-Schleife.
+    /// </summary>
+    class ForeachLoop : IBenchmarkable
+    {
+        public string Name { get { return "foreach-Schleife"; } }
+
+        public void Run(uint levelOfLoad)
+        {
+            var data = new int[levelOfLoad];
+            int count = 0;
+            foreach (int x in data)
+            {
+                if (x == 0)
+                {
+                    ++count;
+                }
+            }
+        }
+    }
+}
